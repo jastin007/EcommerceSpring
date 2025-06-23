ec549999b291b4e637552fc657060aec671112af
Jastin Samuvel A <arun.kumar@gmail.com>
2025-06-23 13:57:42 +0530

    Added basic controller

---
+++ src/main/java/com/example/demo/Controller/CategoryController.java
@@ -0,0 +1,24 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/categories")
+public class CategoryController {
+
+    @GetMapping
+    public String getCategory(){
+        return "Electronics";
+    }
+    @PostMapping
+    public String getPostCategory(){
+        return "Post Electronics";
+    }
+    @GetMapping("/count")
+    public int getCategoryCount(){
+         return 7;
+    }
+}
